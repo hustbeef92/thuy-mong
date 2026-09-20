16227b2ce1a4526439a425c324b942e4d729e8ab
quangminh <nqm150906@gmail.com>
2026-09-20 23:08:39 +0700

    Auto deploy update

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,8 @@
 @echo off
 set "PATH=%USERPROFILE%\AppData\Local\Microsoft\WinGet\Packages\Git.MinGit_Microsoft.Winget.Source_8wekyb3d8bbwe\cmd;%USERPROFILE%\AppData\Local\Microsoft\WinGet\Packages\Git.MinGit_Microsoft.Winget.Source_8wekyb3d8bbwe\mingw64\bin;%PATH%"
 echo Dang day code len GitHub de Vercel deploy...
+git add .
+git commit -m "Auto deploy update"
 git push origin main
 if %ERRORLEVEL% equ 0 (
     echo.
