921bc80858aabb870c71e8f8cf8aa13c2c7cbe79
quangminh <nqm150906@gmail.com>
2026-09-20 21:29:17 +0700

    clean

--- run-push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "PATH=%USERPROFILE%\AppData\Local\Microsoft\WinGet\Packages\Git.MinGit_Microsoft.Winget.Source_8wekyb3d8bbwe\cmd;%PATH%"
-git add -A
-git commit -m "feat: add clear test orders and reset dummy data"
-git push origin main
